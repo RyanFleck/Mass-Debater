28864c40989326849aadf64b84ac7949e74296e6
Ryan Fleck <ryan.fleck@protonmail.com>
2019-02-09 16:31:45 -0500

    Bare Socket.IO webapp.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Mass-Debater
+
 Socket.IO room-based debate platform. 
+
+Live at <https://mass-debater.herokuapp.com/>
--- app.js
+++ app.js
@@ -1,20 +1,110 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const socketio = require('socket.io');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const debug = require('debug')('Mass-Debater:server');
+const helmet = require('helmet');
 
-var users = require('./routes/users');
-
-var app = express();
+const app = express();
+const srv = require('http').Server(app);
+const io = socketio(srv);
 
 app.use(bodyParser.json());
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
+app.use(express.static('static/client_resources'));
 
-app.use('/api/v1/users', users);
-
-app.get('/', (req,res)=>{
-    res.sendFile(path.join( __dirname + '/static/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/index.html'));
 });
+
+app.get('/:room', (req, res) => {
+    res.send(req.params.room + ' debate room');
+});
+
+io.on('connection', (socket)=>{
+    console.log('A user connected.');
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+srv.listen(port);
+srv.on('error', onError);
+srv.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+    // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+    // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP srv "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+    case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+    case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+    default:
+        throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP srv "listening" event.
+ */
+
+function onListening() {
+    var addr = srv.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
 
 module.exports = app;
